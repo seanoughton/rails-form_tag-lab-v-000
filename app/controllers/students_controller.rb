f79f7f95559f9d543c07ef4194ad57b0baaa9cd7
Sean Oughton <seanoughton@gmail.com>
2018-04-19 19:00:23 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,6 @@ class StudentsController < ApplicationController
   end
 
   def create
-    binding.pry
     #session[:form_params] = params.inspect
     Student.create(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to students_path
